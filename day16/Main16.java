6ff9ce55af552d5f3e0a8f527ec08d4ad7d749d7
gozde <kcr.gozde@gmail.com>
2026-04-29 18:16:53 +0300

    fix

--- day16/Main16.java
+++ day16/Main16.java
@@ -2,6 +2,6 @@ package day16;
 
 public class Main16 {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java ...");
     }
 }
